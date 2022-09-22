33f6f4f853141bfb9d476e8f8cde3274d8d8c7e7
gritty-git <62741960+gritty-git@users.noreply.github.com>
2022-09-22 23:01:03 +0530

    intitialised react and server folders

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cors = require('cors')
+const path = require('path');
+const bodyParser = require('body-parser');
+
+
+dotenv.config();
+const app = express();
+
+
+app.use(express.json())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is Running');
+    });
+}
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, function () {
+    console.log("Server running at " + PORT);
+})
