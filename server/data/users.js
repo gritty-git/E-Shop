84b47cb0aeb5398bad748f23e9ff05485cdc63a0
gritty-git <62741960+gritty-git@users.noreply.github.com>
2022-09-22 23:44:10 +0530

    added sample data and seeder script

---
+++ server/data/users.js
@@ -0,0 +1,32 @@
+const bcrypt = require('bcryptjs');
+
+const users = [
+    {
+        name: 'M Soumya Prakash Sahoo',
+        email: 'msoumya.iitbhu@gmail.com',
+        password: bcrypt.hashSync('likuliku', 10)
+
+    },
+    {
+        name: 'ASDFGHJKL',
+        email: 'asdfghjkl@example.com',
+        password: bcrypt.hashSync('123456', 10)
+    },
+    {
+        name: 'QWERTYUIOP',
+        email: 'qwertyuiop@example.com',
+        password: bcrypt.hashSync('123456', 10)
+    },
+    {
+        name: 'QWERTY',
+        email: 'qwerty@example.com',
+        password: bcrypt.hashSync('123456', 10)
+    },
+    {
+        name: 'ASDFG',
+        email: 'asdfg@example.com',
+        password: bcrypt.hashSync('123456', 10)
+    }
+];
+
+module.exports = users;
