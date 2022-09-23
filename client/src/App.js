de72b9e4212e67ddd49cc4fe8084a430c50357b7
gritty-git <62741960+gritty-git@users.noreply.github.com>
2022-09-23 09:09:11 +0530

    setup react routing in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Cart from './pages/Cart';
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <h1> Header </h1>
+      <div className='container'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/search/:searchterm" element={<Search />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 }
