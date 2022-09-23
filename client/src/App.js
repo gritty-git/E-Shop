cd98e804b2e1195c867efeecdd157c1ecde06986
gritty-git <62741960+gritty-git@users.noreply.github.com>
2022-09-23 10:38:27 +0530

    added header footer loader

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,30 @@ import Login from './pages/Login';
 import Cart from './pages/Cart';
 import { Route, Routes } from "react-router-dom"
 
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Profile from './pages/Profile';
+import UserList from './pages/UserList';
+
+
 function App() {
   return (
     <div className="App">
-      <h1> Header </h1>
+      <Header />
+
       <div className='container'>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/userlist" element={<UserList />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/search/:searchterm" element={<Search />} />
           <Route path="*" element={<Home />} />
         </Routes>
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+
+function Header() {
+    const navigate = useNavigate();
+    const [searchTerm, setSearchTerm] = useState("");
+    function inputSubmit(e) {
+        e.preventDefault();
+        navigate(`/search/${searchTerm}`, { state: {} });
+        setSearchTerm("");
+    }
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{ "margin": "0rem 1rem" }}>
+            <NavLink className="navbar-brand" to="/">E - Shop</NavLink>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{ "justifyContent": "space-evenly" }}>
+                <ul className="navbar-nav mr-auto" style={{ "justifyContent": "space-evenly" }}>
+                    <li className="nav-item active">
+                        <NavLink className="nav-link" to="/">Home</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/login/" >Login</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/register">Register</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="/cart">Cart</NavLink>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Profile
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <NavLink className="dropdown-item" to="/profile">View Profile</NavLink>
+                            <NavLink className="dropdown-item" to="/userlist">View All Users</NavLink>
+                            <NavLink className="dropdown-item" to="/login">Sign Out</NavLink>
+
+                        </div>
+                    </li>
+                </ul>
+                <div className="form-inline my-2 my-lg-0">
+                    <form style={{ "float": "left", "margin": "0rem 0.4rem" }} onSubmit={inputSubmit} >
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={(e) => setSearchTerm(e.target.value)} value={searchTerm} />
+                    </form>
+                    <div style={{ "float": "left", "margin": "0rem 0.4rem" }}>
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="button" onClick={inputSubmit}>Search</button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Header;
