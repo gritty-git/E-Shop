d125aa92f286a09bd3b8df2079adb65e1f1f49ec
gritty-git <62741960+gritty-git@users.noreply.github.com>
2022-09-24 01:16:40 +0530

    implemented user register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,3 +1,120 @@
-export default function Register() {
-    return <h1>Register</h1>;
+import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Loader from '../components/Loader';
+import axios from 'axios';
+import FormContainer from '../components/FormContainer';
+import { authActions } from '../store/auth';
+
+const Register = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [message, setMessage] = useState(null)
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const isAuthenticated = useSelector((state) => state.auth.authenticated);
+    const loading = useSelector((state) => state.auth.loading);
+
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigate('/');
+        }
+    }, [isAuthenticated])
+
+    const submitHandler = async (e) => {
+        e.preventDefault()
+        if (password !== confirmPassword) {
+            setMessage('Passwords do not match')
+        } else {
+            dispatch(authActions.authRequest());
+            try {
+                const config = {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+                const { data } = await axios.post(
+                    '/api/users',
+                    { name, email, password },
+                    config
+                )
+                dispatch(authActions.signIn(data));
+                localStorage.setItem('userInfo', JSON.stringify(data));
+            } catch (error) {
+                dispatch(authActions.authRequestClose());
+                setMessage(error.response.data.message);
+            }
+
+        }
+    }
+
+    return (
+        <FormContainer>
+            <h1>Sign Up</h1>
+            {message && <p>{message}</p>}
+
+            {loading && <Loader />}
+            <Form onSubmit={submitHandler}>
+                <Form.Group controlId='name'>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        type='name'
+                        placeholder='Enter name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='email'>
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control
+                        type='email'
+                        placeholder='Enter email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='password'>
+                    <Form.Label>Password Address</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Enter password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='confirmPassword'>
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Confirm password'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+
+                <Button type='submit' className='mt-4' variant='primary'>
+                    Register
+                </Button>
+            </Form>
+
+            <Row className='py-3'>
+                <Col>
+                    Have an Account?{' '}
+                    <Link to='/login'>
+                        Login
+                    </Link>
+                </Col>
+            </Row>
+        </FormContainer>
+    )
 }
+
+export default Register;
