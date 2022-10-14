c822caafe2f594440734c9d435c8ab92045e7be6
gritty-git <62741960+gritty-git@users.noreply.github.com>
2022-10-14 12:47:39 +0530

    added more order routes

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,10 +1,17 @@
 const asyncHandler = require('express-async-handler');
 const Order = require('../models/orderModel');
 
+// @desc    Create new order
+// @route   POST /api/orders
+// @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
     const {
-        cartItems,
+        orderItems,
         shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
         totalPrice,
     } = req.body
 
@@ -14,18 +21,25 @@ const addOrderItems = asyncHandler(async (req, res) => {
 
     } else {
         const order = new Order({
-            orderItems: cartItems,
+            orderItems,
             user: req.user._id,
             shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
             totalPrice,
         })
-        console.log(order);
+        //console.log(order);
         const createdOrder = await order.save()
 
         res.status(201).json(createdOrder)
     }
 })
 
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.id).populate(
         'user',
@@ -40,9 +54,37 @@ const getOrderById = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if (order) {
+        order.isPaid = true
+        order.paidAt = Date.now()
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address,
+        }
+
+        const updatedOrder = await order.save()
+
+        res.json(updatedOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
+// @desc    Get logged in user orders
+// @route   GET /api/orders/myorders
+// @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
     const orders = await Order.find({ user: req.user._id })
     res.json(orders)
 })
 
-module.exports = { addOrderItems, getOrderById, getMyOrders }
+module.exports = { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders }
